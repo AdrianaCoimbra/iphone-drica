2064bd20e0b38408bcefc50e2b58cadb02fea540
AdrianaCoimbra <90111107+AdrianaCoimbra@users.noreply.github.com>
2023-03-17 13:35:43 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function trocaCor(cor){
+let circulo = document.querySelector('.circulo')
+circulo . style.background = cor
+}
+function trocaImagem(img){
+    let imgIphone = document.querySelector('.iphone')
+imgIphone . src = img
+}
